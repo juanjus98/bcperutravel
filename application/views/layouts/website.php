7bfeef12cb4917db83eb01697a0031b528ffdc3c
Juan Julio Sandoval Layza <juanjus98@gmail.com>
2017-08-21 18:38:55 -0500

    Página de inicio - menú, slide, busqueda

--- application/views/layouts/website.php
+++ application/views/layouts/website.php
@@ -75,159 +75,206 @@ $tag_image = $head_info['image'];
 <script type="text/javascript">var base_url='<?php echo base_url();?>';</script>
 </head>
 <body>
-<div id="header_1">
- <header class="">
- <div id="top_line">
- <div class="container bg-pattern-1">
- <div class="row">
- <div class="col-md-6 col-sm-6 col-xs-12">
-  <a href="tel://+512630012">+51 263 0012</a>
- </div>
- <div class="col-md-6 col-sm-6 hidden-xs">
- <ul id="top_links" class=""><li id="menu-item-164" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-164"><a href="#">Wishlist</a></li>
-<li id="menu-item-165" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-165"><a href="#0">PURCHASE THIS TEMPLATE</a></li>
-</ul>
- </div>
- </div>
- <!-- End row -->
- </div>
- <!-- End container-->
- </div>
- <!-- End top line-->
-<!-- navbar-->
-<div class="wa-menu">
- <nav class="navbar navbar-website navbar-static-top">
- <div class="container">
- <div class="navbar-header">
- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
- <span class="sr-only">Toggle navigation</span>
- <span class="icon-bar"></span>
- <span class="icon-bar"></span>
- <span class="icon-bar"></span>
- </button>
- <a class="navbar-brand" href="<?php echo base_url();?>">
- <img src="<?php echo base_url('assets/images/logo.png');?>" alt="">
- </a>
- </div>
- <div id="navbar3" class="navbar-collapse collapse">
- <ul class="nav navbar-nav navbar-right">
- <li class="active"><a href="#">Home</a></li>
- <li><a href="#">About</a></li>
- <li><a href="#">Contact</a></li>
- <li class="dropdown">
- <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
- <ul class="dropdown-menu" role="menu">
- <li><a href="#">Action</a></li>
- <li><a href="#">Another action</a></li>
- <li><a href="#">Something else here</a></li>
- <li class="divider"></li>
- <li class="dropdown-header">Nav header</li>
- <li><a href="#">Separated link</a></li>
- <li><a href="#">One more separated link</a></li>
- </ul>
- </li>
- </ul>
- </div>
- <!--/.nav-collapse -->
- </div>
- <!--/.container-fluid -->
+  <div id="header_1">
+   <header class="">
+     <div id="top_line">
+       <div class="container bg-pattern-1">
+         <div class="row">
+           <div class="col-md-6 col-sm-6 col-xs-12">
+            <ul class="nav nav-pills nav-topline">
+              <li role="presentation"><a href="tel://+512630012"><i class="fa fa-phone" aria-hidden="true"></i>+51 263 0012</a></li>
+              <li role="presentation"><a href="#" class="whatsapp"><i class="fa fa-whatsapp" aria-hidden="true"></i>+51 981 001 581</a></li>
+            </ul>
+          </div>
+          <div class="col-md-6 col-sm-6 hidden-xs">
+           <ul class="nav nav-pills nav-topline pull-right">
+            <li role="presentation"><a href="#">Nosotros</a></li>
+            <li role="presentation"><a href="#">Blog</a></li>
+            <li role="presentation"><a href="#">Contáctenos</a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- End row -->
+    </div>
+    <!-- End container-->
+  </div>
+  <!-- End top line-->
+  <!-- navbar-->
+  <div class="wa-menu">
+   <nav class="navbar navbar-website navbar-static-top">
+     <div class="container">
+       <div class="navbar-header">
+         <button type="button" class="navbar-toggle x collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar top-bar"></span>
+          <span class="icon-bar middle-bar"></span>
+          <span class="icon-bar bottom-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo base_url();?>">
+         <img src="<?php echo base_url('assets/images/logo.png');?>" alt="">
+       </a>
+     </div>
+     <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1">
+       <ul class="nav navbar-nav navbar-right navbar-nav-wa">
+         <li class="active"><a href="#">Inicio</a></li>
+         <li><a href="#">Paquetes Turísticos</a></li>
+         <li><a href="#">Promociones</a></li>
+         <li><a href="#">Tickets</a></li>
+         <li><a href="#">Año Nuevo</a></li>
+       </ul>
+     </div>
+     <!--/.nav-collapse -->
+   </div>
+   <!--/.container-fluid -->
  </nav>
 </div>
 <!-- //navbar-->
- </header>
- <!-- End Header -->
- </div>
+</header>
+<!-- End Header -->
+</div>
 <!-- Carousel-->
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="carousel-home" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
-  <ol class="carousel-indicators">
+  <!-- <ol class="carousel-indicators">
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
     <li data-target="#carousel-example-generic" data-slide-to="1"></li>
     <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
+  </ol> -->
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
       <img src="<?php echo base_url('assets/images/slide-1.jpg');?>" alt="Slide 1">
-      <div class="carousel-caption">
+      <!-- <div class="carousel-caption">
         Captión 01
-      </div>
+      </div> -->
     </div>
     <div class="item">
       <img src="<?php echo base_url('assets/images/slide-2.jpg');?>" alt="Slide 2">
-      <div class="carousel-caption">
+      <!-- <div class="carousel-caption">
         Captión 02
-      </div>
+      </div> -->
     </div>
     <div class="item">
       <img src="<?php echo base_url('assets/images/slide-3.jpg');?>" alt="Slide 3">
-      <div class="carousel-caption">
+      <!-- <div class="carousel-caption">
         Captión 03
-      </div>
+      </div> -->
     </div>
 
   </div>
 
   <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+  <!-- <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
   <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
-  </a>
+  </a> -->
+
+  <div class="general-caption">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1>Disfruta de una experiencia perfecta</h1>
+          <p>Encuentra los mejores tours al mejor precio.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="cont-busqueda">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="tab" role="tabpanel">
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">
+              <li role="presentation" class="active"><a href="#Section1" aria-controls="home" role="tab" data-toggle="tab"><span>1</span>Section</a></li>
+              <li role="presentation"><a href="#Section2" aria-controls="profile" role="tab" data-toggle="tab"><span>2</span>Section</a></li>
+              <li role="presentation"><a href="#Section3" aria-controls="messages" role="tab" data-toggle="tab"><span>3</span>Section</a></li>
+              
+            </ul>
+            <!-- Tab panes -->
+            <div class="tab-content">
+              <div role="tabpanel" class="tab-pane active" id="Section1">
+                <h3>Web Development</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut maximus purus, quis condimentum odio. Pellentesque lorem ex, tempor sit amet justo.
+                </p>
+              </div>
+              <div role="tabpanel" class="tab-pane" id="Section2">
+                <h3>Brand Building</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc semper accumsan urna, nec faucibus sem sagittis id. Pellentesque finibus, eros a aliquet ornare, mi est ullamcorper neque, eu accumsan justo massa nec elit. Sed ac leo et erat gravida maximus.
+                </p>
+              </div>
+              <div role="tabpanel" class="tab-pane" id="Section3">
+                <h3>Responsive Design</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum sed diam ac fermentum. Mauris nec pellentesque neque. Cras nec diam euismod, congue sapien eu, fermentum libero. Vestibulum quis sem.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
 <!-- //Carousel-->
 <div class="container">
-<!-- body-->
-<?php echo @$template['body']; ?>
-<!-- //body-->
+  <!-- body-->
+  <?php echo @$template['body']; ?>
+  <!-- //body-->
 </div>
 <!-- Footer-->
 <footer>
-<section class="nb-footer">
- <div class="row">
- <div class="col-md-4 col-sm-6">
- <div class="footer-single">
- <div class="footer-title"><h2>Información de Contácto</h2></div>
- <address>
- <strong>Oficina:</strong> <?php echo $retVal = (!empty($direccion)) ? $direccion : '' ; ?> <br>
- <i class="fa fa-phone"></i> <?php echo $retVal = (!empty($telefono_1)) ? $telefono_1 : '' ; ?> <br>
- <i class="fa fa-whatsapp"></i> <?php echo $retVal = (!empty($telefono_2)) ? $telefono_2 : '' ; ?><br>
- <i class="fa fa-envelope"></i> <?php echo $retVal = (!empty($email_1)) ? $email_1 : '' ; ?><br>
- </address> 
- </div>
- </div>
- <div class="col-md-4 col-sm-6">
- <div class="footer-single useful-links">
- <div class="footer-title"><h2>Tours Destacados</h2></div>
- <ul class="list-unstyled">
- <li><a href="#">Home <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">About Us <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">Services <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">Portfolio <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">Pricing <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">Contact Us <i class="fa fa-angle-right pull-right"></i></a></li>
- </ul>
- </div>
- </div>
- <div class="col-md-4 col-sm-6">
- <div class="footer-single useful-links">
- <div class="footer-title"><h2>Estadia</h2></div>
- <ul class="list-unstyled">
- <li><a href="#">Home <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">About Us <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">Services <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">Portfolio <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">Pricing <i class="fa fa-angle-right pull-right"></i></a></li>
- <li><a href="#">Contact Us <i class="fa fa-angle-right pull-right"></i></a></li>
- </ul>
- </div>
- </div>
- </div>
-</section>
+  <section class="nb-footer">
+   <div class="row">
+     <div class="col-md-4 col-sm-6">
+       <div class="footer-single">
+         <div class="footer-title"><h2>Información de Contácto</h2></div>
+         <address>
+           <strong>Oficina:</strong> <?php echo $retVal = (!empty($direccion)) ? $direccion : '' ; ?> <br>
+           <i class="fa fa-phone"></i> <?php echo $retVal = (!empty($telefono_1)) ? $telefono_1 : '' ; ?> <br>
+           <i class="fa fa-whatsapp"></i> <?php echo $retVal = (!empty($telefono_2)) ? $telefono_2 : '' ; ?><br>
+           <i class="fa fa-envelope"></i> <?php echo $retVal = (!empty($email_1)) ? $email_1 : '' ; ?><br>
+         </address> 
+       </div>
+     </div>
+     <div class="col-md-4 col-sm-6">
+       <div class="footer-single useful-links">
+         <div class="footer-title"><h2>Tours Destacados</h2></div>
+         <ul class="list-unstyled">
+           <li><a href="#">Home <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">About Us <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">Services <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">Portfolio <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">Pricing <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">Contact Us <i class="fa fa-angle-right pull-right"></i></a></li>
+         </ul>
+       </div>
+     </div>
+     <div class="col-md-4 col-sm-6">
+       <div class="footer-single useful-links">
+         <div class="footer-title"><h2>Estadia</h2></div>
+         <ul class="list-unstyled">
+           <li><a href="#">Home <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">About Us <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">Services <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">Portfolio <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">Pricing <i class="fa fa-angle-right pull-right"></i></a></li>
+           <li><a href="#">Contact Us <i class="fa fa-angle-right pull-right"></i></a></li>
+         </ul>
+       </div>
+     </div>
+   </div>
+ </section>
 </footer>
 <!-- //Footer-->
 <!-- JavaScript-->
@@ -256,6 +303,6 @@ $tag_image = $head_info['image'];
 <!-- //select2-->
 <script src="<?php echo base_url('assets/js/website.min.js');?>" type="text/javascript"></script>
 <!-- JavaScript-->
-<link href="https://fonts.googleapis.com/css?family=Lato:300,400|Lobster" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Lobster" rel="stylesheet">
 </body>
 </html>
