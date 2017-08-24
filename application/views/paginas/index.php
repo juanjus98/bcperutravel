0f7db9d10eec9eb2dcfe8017a74834a01db1e630
Juan Julio Sandoval Layza <juanjus98@gmail.com>
2017-08-24 18:30:37 -0500

    Carousel de promociones

--- application/views/paginas/index.php
+++ application/views/paginas/index.php
@@ -1,1 +1,256 @@
-Main
+<!-- Carousel-->
+<div id="carousel-home" class="carousel slide" data-ride="carousel">
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+		<div class="item active">
+			<img src="<?php echo base_url('assets/images/slide-1.jpg');?>" alt="Slide 1">
+		</div>
+		<div class="item">
+			<img src="<?php echo base_url('assets/images/slide-2.jpg');?>" alt="Slide 2">
+		</div>
+		<div class="item">
+			<img src="<?php echo base_url('assets/images/slide-3.jpg');?>" alt="Slide 3">
+		</div>
+	</div>
+
+	<div class="general-caption">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1>Disfruta de una experiencia perfecta</h1>
+					<p>Encuentra los mejores tours al mejor precio.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="cont-busqueda">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="tab" role="tabpanel">
+						<!-- Nav tabs -->
+						<ul class="nav nav-tabs" role="tablist">
+							<li role="presentation" class="active"><a href="#seccion-1" aria-controls="paquetes-turisticos" role="tab" data-toggle="tab">Paquetes turísticos</a></li>
+							<li role="presentation"><a href="#seccion-2" aria-controls="tickets" role="tab" data-toggle="tab">Tickets</a></li>              
+						</ul>
+						<!-- Tab panes -->
+						<div class="tab-content">
+							<div role="tabpanel" class="tab-pane fade in active" id="seccion-1">
+								<form class="form-inline">
+									<div class="form-group">
+										<label class="sr-only" for="origen">Origen</label>
+										<input type="text" class="form-control" name="origen" id="origen" placeholder="Origen">
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="destino">Origen</label>
+										<input type="text" class="form-control" name="destino" id="destino" placeholder="Destino">
+									</div>
+
+									<div class="form-group">
+										<label class="sr-only" for="partida">Partida</label>
+										<input type="text" class="form-control" name="partida" id="partida" placeholder="Partida">
+									</div>
+
+									<div class="form-group">
+										<label class="sr-only" for="regreso">Regreso</label>
+										<input type="text" class="form-control" name="regreso" id="regreso" placeholder="Regreso">
+									</div>
+
+									<button type="submit" class="btn btn-primary-1">Buscar</button>
+								</form>
+							</div>
+							<div role="tabpanel" class="tab-pane fade" id="seccion-2">
+								<form class="form-inline">
+									<div class="form-group">
+										<label class="sr-only" for="origen">Origen</label>
+										<input type="text" class="form-control" name="origen" id="origen" placeholder="Origen">
+									</div>
+									<div class="form-group">
+										<label class="sr-only" for="destino">Origen</label>
+										<input type="text" class="form-control" name="destino" id="destino" placeholder="Destino">
+									</div>
+
+									<div class="form-group">
+										<label class="sr-only" for="partida">Partida</label>
+										<input type="text" class="form-control" name="partida" id="partida" placeholder="Partida">
+									</div>
+
+									<div class="form-group">
+										<label class="sr-only" for="regreso">Regreso</label>
+										<input type="text" class="form-control" name="regreso" id="regreso" placeholder="Regreso">
+									</div>
+
+									<button type="submit" class="btn btn-primary-1">Buscar</button>
+								</form>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+<!-- //Carousel-->
+
+<section class="wrapper">
+	<div class="container container-principal">
+		<div class="divider_border"></div>
+		<div class="cont-main">
+			<!--Contenido website cada página-->
+			<div class="cont-promos tabbable-line">
+				<h3>Promociones</h3>
+				<div class="clearfix"></div>
+				<!-- Nav tabs -->
+				<ul class="nav nav-tabs nav-justified" role="tablist">
+					<li role="presentation" class="active"><a href="#tab-promo-1" aria-controls="tab-promo-1" role="tab" data-toggle="tab">Home</a></li>
+					<li role="presentation"><a href="#tab-promo-2" aria-controls="tab-promo-2" role="tab" data-toggle="tab">Profile</a></li>
+					<li role="presentation"><a href="#tab-promo-3" aria-controls="tab-promo-3" role="tab" data-toggle="tab">Messages</a></li>
+					<li role="presentation"><a href="#tab-promo-4" aria-controls="tab-promo-4" role="tab" data-toggle="tab">Settings</a></li>
+				</ul>
+
+				<!-- Tab panes -->
+				<div class="tab-content">
+					<div role="tabpanel" class="tab-pane fade in active" id="tab-promo-1">
+						<ul id="carousel-promo-1" class="carousel-promo">
+							<li>
+								<div class="thumbnail thumbnail-item">
+									<a href="#">
+										<img src="https://unsplash.it/800/600?image=59" alt="Alt aquí">
+									</a>
+									<div class="caption">
+										<h3>Titulo INICIO</h3>
+										<p>Descripción</p>
+										<p>
+											<a href="#" class="btn btn-primary" role="button">Button</a> 
+											<a href="#" class="btn btn-default" role="button">Button</a>
+										</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div class="thumbnail thumbnail-item">
+									<a href="#">
+										<img src="https://unsplash.it/800/600?image=59" alt="Alt aquí">
+									</a>
+									<div class="caption">
+										<h3>Titulo</h3>
+										<p>Descripción</p>
+										<p>
+											<a href="#" class="btn btn-primary" role="button">Button</a> 
+											<a href="#" class="btn btn-default" role="button">Button</a>
+										</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div class="thumbnail thumbnail-item">
+									<a href="#">
+										<img src="https://unsplash.it/800/600?image=59" alt="Alt aquí">
+									</a>
+									<div class="caption">
+										<h3>Titulo</h3>
+										<p>Descripción</p>
+										<p>
+											<a href="#" class="btn btn-primary" role="button">Button</a> 
+											<a href="#" class="btn btn-default" role="button">Button</a>
+										</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div class="thumbnail thumbnail-item">
+									<a href="#">
+										<img src="https://unsplash.it/800/600?image=59" alt="Alt aquí">
+									</a>
+									<div class="caption">
+										<h3>Titulo</h3>
+										<p>Descripción</p>
+										<p>
+											<a href="#" class="btn btn-primary" role="button">Button</a> 
+											<a href="#" class="btn btn-default" role="button">Button</a>
+										</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div class="thumbnail thumbnail-item">
+									<a href="#">
+										<img src="https://unsplash.it/800/600?image=59" alt="Alt aquí">
+									</a>
+									<div class="caption">
+										<h3>Titulo</h3>
+										<p>Descripción</p>
+										<p>
+											<a href="#" class="btn btn-primary" role="button">Button</a> 
+											<a href="#" class="btn btn-default" role="button">Button</a>
+										</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div class="thumbnail thumbnail-item">
+									<a href="#">
+										<img src="https://unsplash.it/800/600?image=59" alt="Alt aquí">
+									</a>
+									<div class="caption">
+										<h3>Titulo</h3>
+										<p>Descripción</p>
+										<p>
+											<a href="#" class="btn btn-primary" role="button">Button</a> 
+											<a href="#" class="btn btn-default" role="button">Button</a>
+										</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div class="thumbnail thumbnail-item">
+									<a href="#">
+										<img src="https://unsplash.it/800/600?image=59" alt="Alt aquí">
+									</a>
+									<div class="caption">
+										<h3>Titulo</h3>
+										<p>Descripción</p>
+										<p>
+											<a href="#" class="btn btn-primary" role="button">Button</a> 
+											<a href="#" class="btn btn-default" role="button">Button</a>
+										</p>
+									</div>
+								</div>
+							</li>
+
+							<li>
+								<div class="thumbnail thumbnail-item">
+									<a href="#">
+										<img src="https://unsplash.it/800/600?image=59" alt="Alt aquí">
+									</a>
+									<div class="caption">
+										<h3>Titulo FIN</h3>
+										<p>Descripción</p>
+										<p>
+											<a href="#" class="btn btn-primary" role="button">Button</a> 
+											<a href="#" class="btn btn-default" role="button">Button</a>
+										</p>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					<div role="tabpanel" class="tab-pane fade" id="tab-promo-2"><h2>2</h2></div>
+					<div role="tabpanel" class="tab-pane fade" id="tab-promo-3"><h2>3</h2></div>
+					<div role="tabpanel" class="tab-pane fade" id="tab-promo-4"><h2>4</h2></div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //section .wrapper -->
--- build/js/website/app.js
+++ build/js/website/app.js
@@ -28,45 +28,16 @@ $(function() {
 	//Toolbar static
 	$("#tool-bar").sticky({ topSpacing: 0 });
 
-	//Galería videos.
-	$("#content-slider").lightSlider({
-		loop:true,
-		auto:true,
-		item:4,
-		slideMove:2,
-		easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-		speed:600,
-		pauseOnHover: true,
-		pager: false,
-		responsive : [
-		{
-			breakpoint:800,
-			settings: {
-				item:3,
-				slideMove:1,
-				slideMargin:6,
-			}
-		},
-		{
-			breakpoint:480,
-			settings: {
-				item:2,
-				slideMove:1
-			}
-		}
-		]
-	});
-
- //Galería de fotos.
- $("#content-slider-fotos").lightSlider({
- 	loop:true,
- 	auto:false,
+	/**
+ * Lightslider
+ */
+ $('#carousel-promo-1').lightSlider({
  	item:4,
+ 	loop:true,
  	slideMove:2,
  	easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
  	speed:600,
- 	pauseOnHover: true,
- 	pager: false,
+ 	slideMargin: 15,
  	responsive : [
  	{
  		breakpoint:800,
@@ -86,18 +57,5 @@ $(function() {
  	]
  });
 
-	//Ver video fancybox
-	$(".various").fancybox({
-		maxWidth : 800,
-		maxHeight : 600,
-		fitToView : false,
-		width : '70%',
-		height : '70%',
-		autoSize : false,
-		closeClick : false,
-		openEffect : 'none',
-		closeEffect : 'none'
-	});
 
-
-	});
+});
