f8aab0e6a4b25c9cdbf595356989bb200e80867a
Juan Julio Sandoval Layza <juanjus98@gmail.com>
2017-11-02 18:30:15 -0500

    Detalles del servicio, y formulario de contacto

--- application/views/paginas/detalle.php
+++ application/views/paginas/detalle.php
@@ -73,32 +73,8 @@ $urlImagen = (!empty($imagen_2)) ? base_url($this->config->item('upload_path') .
           </div>
           <div class="clearfix"></div>
 
-          <div class="cont-tabs">
-
-            <!-- Nav tabs -->
-            <ul class="nav nav-tabs" role="tablist">
-              <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-              <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-              <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-              <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
-            </ul>
-
-            <!-- Tab panes -->
-            <div class="tab-content">
-              <div role="tabpanel" class="tab-pane active" id="home">
-                <h1>Home</h1>
-              </div>
-              <div role="tabpanel" class="tab-pane" id="profile">
-                <h1>Profile</h1>
-              </div>
-              <div role="tabpanel" class="tab-pane" id="messages">
-                <h1>Messages</h1>
-              </div>
-              <div role="tabpanel" class="tab-pane" id="settings">
-                <h1>Setings</h1>
-              </div>
-            </div>
-
+          <div class="cont-descripcion">
+            <?php echo $descripcion;?>
           </div>
 
         </div>
